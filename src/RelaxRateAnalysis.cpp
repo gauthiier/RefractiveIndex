fe35f843efdcc8218fb15f2534e0db9d2448998e
Jamie Allen <jamie@heavyside.net>
2012-02-22 16:20:40 +0100

    cvGrayDiff1 was not being generated properly in RelaxRate - fixed

--- src/RelaxRateAnalysis.cpp
+++ src/RelaxRateAnalysis.cpp
@@ -117,12 +117,10 @@ void RelaxRateAnalysis::synthesise()
                 cvGrayImage1 = cvColorImage1;
                 cvGrayImage2 = cvColorImage2;
                 
-                //cvGrayDiff1.absDiff(cvGrayImage2, cvGrayImage1);
+                cvGrayDiff1.absDiff(cvGrayImage2, cvGrayImage1);
                 cvGrayDiff1.erode();
                 cvGrayDiff1.contrastStretch();
                 cvGrayDiff1.blur(5);
-                //cvGrayDiff1.dilate();
-                //cvGrayDiff1.contrastStretch();
                 
                 /////////////////////////////////// SAVE TO DISK IN THE SYNTHESIS FOLDER ////////////////////////////////
                 string file_name;
