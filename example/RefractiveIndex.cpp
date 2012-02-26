60e51a84f2862eaca33ffac684e7922045afa0b7
Jamie Allen <jamie@heavyside.net>
2012-02-26 16:58:18 +0100

    this code has consolidated save function in abstract analysis - un-commented the analyses so they all run -

--- example/RefractiveIndex.cpp
+++ example/RefractiveIndex.cpp
@@ -100,7 +100,7 @@ void RefractiveIndex::setup()
     _analysisVector.push_back(new ShadowScapesAnalysis(H));
     _analysisVector.push_back(new ShadowScapesAnalysis(D));
     
-    /*
+    
     _analysisVector.push_back(new RelaxRateAnalysis());
     
     _analysisVector.push_back(new IResponseAnalysis());
@@ -117,7 +117,7 @@ void RefractiveIndex::setup()
     
     _analysisVector.push_back(new DiffNoiseAnalysis());
 
-     */
+
     //_currentAnalysisIndx = 0;
     //_currentAnalysis = _analysisVector.at(_currentAnalysisIndx++); 
     //_state = ISTATE_START;
